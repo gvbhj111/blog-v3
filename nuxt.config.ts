608f994a1d701d16f76d9a8bac73e50f736f4585
gvbhj111 <gvbhj2019@163.com>
2025-05-04 15:18:01 +0800

    wss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,76 +1,85 @@
-import process from 'node:process'
-import blogConfig, { routeRules } from './blog.config'
+import type { BundledTheme } from "shiki";
+import blogConfig from "./blog.config";
+import remarkGfm from "remark-gfm";
+import remarkReadingTime from "remark-reading-time";
 
-// 此处配置无需修改
+const shikiOptions = {
+    langs: blogConfig.fileExtensions,
+    themes: <BundledTheme[]>["catppuccin-latte", "one-dark-pro"],
+    defaultTheme: <BundledTheme>"catppuccin-latte",
+    darkTheme: <BundledTheme>"one-dark-pro",
+};
+
 export default defineNuxtConfig({
+    devtools: { enabled: true },
     app: {
+        rootId: "lab-root",
         head: {
             htmlAttrs: {
                 lang: blogConfig.language,
             },
             link: [
-                { rel: 'icon', href: blogConfig.favicon },
-                { rel: 'alternate', type: 'application/atom+xml', href: '/atom.xml' },
-                { rel: 'preconnect', href: blogConfig.twikoo.preload },
-
-                { rel: 'preconnect', href: 'https://cdn-font.hyperos.mi.com' },
+                {
+                    rel: "icon",
+                    href: "https://cdn.jsdelivr.net/gh/Labmem-00/img-hosting@main/LabBlog/favicon.ico",
+                },
+                {
+                    rel: "alternate",
+                    type: "application/atom+xml",
+                    href: "/atom.xml",
+                },
+                { rel: "preconnect", href: "https://cdn-font.hyperos.mi.com" },
                 // 浏览器渲染中文 VF 字重有问题
-                { rel: 'stylesheet', href: 'https://cdn-font.hyperos.mi.com/font/css?family=MiSans_VF:VF:Chinese_Simplify,Latin&display=swap', media: 'none', onload: 'this.media="all"' },
                 // https://cdn-font.hyperos.mi.com/font/css?family=MiSans:100,200,300,400,450,500,600,650,700,900:Chinese_Simplify,Latin&display=swap
-
-                { rel: 'preconnect', href: 'https://fonts.googleapis.cn' },
-                { rel: 'preconnect', href: 'https://fonts.gstatic.cn', crossorigin: '' },
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap', media: 'none', onload: 'this.media="all"' },
-                // Fira Code 没有斜体
-                // { rel: 'stylesheet', href: 'https://fonts.googleapis.cn/css2?family=Fira+Code:wght@300..700&family=Noto+Serif+SC:wght@200..900&display=swap', media: 'none', onload: 'this.media="all"' },
-
-                // { rel: 'stylesheet', href: 'https://gcore.jsdelivr.net/npm/nerdfonts-web/nf.min.css' },
-            ],
-            meta: [
-                { name: 'author', content: `${blogConfig.author.name} <${blogConfig.author.email}>` },
-                // 此处为元数据的生成器标识，不建议修改
-                { 'name': 'generator', 'content': 'blog-v3', 'data-github-repo': 'https://github.com/L33Z22L11/blog-v3' },
+                {
+                    rel: "stylesheet",
+                    href: "https://cdn-font.hyperos.mi.com/font/css?family=MiSans_VF:VF:Chinese_Simplify,Latin&display=swap",
+                    media: "none",
+                    onload: 'this.media="all"',
+                },
+                { rel: "preconnect", href: "https://fonts.googleapis.cn" },
+                { rel: "preconnect", href: "https://fonts.gstatic.cn" },
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.cn/css2?family=Fira+Code:wght@300..700&family=Noto+Serif+SC:wght@200..900&display=swap",
+                    media: "none",
+                    onload: 'this.media="all"',
+                },
             ],
             templateParams: {
-                separator: '|',
+                separator: "|",
             },
             titleTemplate: `%s %separator ${blogConfig.title}`,
-            script: blogConfig.scripts,
         },
-        rootId: 'z-root',
     },
 
-    compatibilityDate: '2024-08-03',
+    compatibilityDate: "2024-08-03",
 
     components: [
-        { path: '~/components/partial', prefix: 'Z' },
-        { path: '~/components/zhilu', prefix: 'Zhilu', global: true },
-        '~/components',
+        { path: "~/components/partial", prefix: "Z" },
+        { path: "~/components/zhilu", prefix: "Zhilu", global: true },
+        "~/components",
     ],
 
     css: [
-        '@/assets/css/animation.scss',
-        '@/assets/css/article.scss',
-        '@/assets/css/color.scss',
-        '@/assets/css/main.scss',
-        '@/assets/css/reusable.scss',
+        "@/assets/styles/article.scss",
+        "@/assets/styles/color.scss",
+        "@/assets/styles/main.scss",
+        "@/assets/styles/reusable.scss",
     ],
 
-    // https://github.com/nuxt/devtools/issues/761
-    // 修了多久了，还整天内存泄漏
-    devtools: {
-        enabled: false,
-    },
-
-    features: {
-        inlineStyles: false,
+    experimental: {
+        viewTransition: true,
     },
 
     future: {
         compatibilityVersion: 4,
     },
 
-    routeRules,
+    routeRules: {
+        // ...redirects,
+        "/api/stats": { prerender: true },
+    },
 
     runtimeConfig: {
         public: {
@@ -82,31 +91,35 @@ export default defineNuxtConfig({
         css: {
             preprocessorOptions: {
                 scss: {
-                    additionalData: '@use "@/assets/css/_variable.scss" as *;',
-                    api: 'modern-compiler',
+                    additionalData: `
+                        @use "@/assets/styles/_variable.scss" as *;
+                        @use "@/assets/styles/_mixin.scss" as *;
+                    `,
+                    api: "modern-compiler",
                 },
             },
         },
-        server: {
-            allowedHosts: true,
-        },
+    },
+
+    vue: {
+        propsDestructure: true,
     },
 
     modules: [
-        '@nuxt/content',
-        '@nuxt/icon',
-        '@nuxt/image',
-        '@nuxtjs/color-mode',
-        '@nuxtjs/seo',
-        '@pinia/nuxt',
-        '@vueuse/nuxt',
-        '@zinkawaii/nuxt-shiki',
+        "@nuxt/content",
+        "@nuxt/icon",
+        "@nuxt/image",
+        "@nuxtjs/color-mode",
+        "@nuxtjs/seo",
+        "@pinia/nuxt",
+        "@vueuse/nuxt",
+        "@zinkawaii/nuxt-shiki",
     ],
 
     colorMode: {
-        preference: 'system',
-        fallback: 'light',
-        classSuffix: '',
+        preference: "system",
+        fallback: "light",
+        classSuffix: "",
     },
 
     content: {
@@ -114,42 +127,47 @@ export default defineNuxtConfig({
             search: {},
         },
         highlight: {
-            langs: blogConfig.shiki.langs,
+            langs: shikiOptions.langs,
             theme: {
-                default: blogConfig.shiki.defaultTheme,
-                dark: blogConfig.shiki.darkTheme,
+                default: shikiOptions.defaultTheme,
+                dark: shikiOptions.darkTheme,
             },
         },
         markdown: {
-            remarkPlugins: ['remark-reading-time'],
-            toc: { depth: 4, searchDepth: 4 },
+            remarkPlugins: [
+                ["remark-reading-time", remarkReadingTime],
+                ['remark-gfm', remarkGfm]
+            ],
+            toc: { depth: 4, searchDepth: 4 }, //目录渲染
         },
     },
 
     icon: {
         customCollections: [
-            { prefix: 'zi', dir: './app/assets/icons' },
+            {
+                prefix: "zi",
+                dir: "https://cdn.jsdelivr.net/gh/Labmem-00/img-hosting@main/LabBlog/favicon.ico",
+            },
         ],
+        // BUG: 首次加载有概率无图标
     },
 
     image: {
-        // Netlify 需要特殊处理
-        provider: process.env.NUXT_IMAGE_PROVIDER,
-        domains: blogConfig.imageDomains,
-        format: ['avif', 'webp'],
+        domains: [],
+        format: ["avif", "webp"],
     },
 
     robots: {
-        disallow: blogConfig.robotsNotIndex,
+        disallow: ["/preview", "/previews/*"],
     },
 
     shiki: {
-        bundledLangs: blogConfig.shiki.bundledLangs,
-        bundledThemes: blogConfig.shiki.themes,
-        defaultLang: 'log',
+        bundledLangs: shikiOptions.langs,
+        bundledThemes: shikiOptions.themes,
+        defaultLang: "log",
         defaultTheme: {
-            light: blogConfig.shiki.defaultTheme,
-            dark: blogConfig.shiki.darkTheme,
+            light: shikiOptions.defaultTheme,
+            dark: shikiOptions.darkTheme,
         },
     },
 
@@ -157,4 +175,4 @@ export default defineNuxtConfig({
         name: blogConfig.title,
         url: blogConfig.url,
     },
-})
+});
