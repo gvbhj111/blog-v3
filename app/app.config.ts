65cce2a4731f938d925cd5aa34b170d180e6c106
lingwang <87105559+gvbhj111@users.noreply.github.com>
2025-05-05 15:07:59 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -99,8 +99,6 @@ export default defineAppConfig({
         {
             title: '',
             items: [
-                { icon: 'ph:files-bold', text: '文章', url: 'https://lingwang.netlify.app/' },
-                { icon: 'ph:link-bold', text: '友链', url: '/link' },
                 { icon: 'ph:archive-bold', text: '归档', url: '/archive' },
             ],
         },
